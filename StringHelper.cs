85c4b20b9d1e4f8aac5ce54d6337cdd77686a9e9
Prystauka Yauheni <34281826+Zheka1389@users.noreply.github.com>
2019-02-12 13:00:31 +0300

    1

--- StringHelper.cs
+++ StringHelper.cs
@@ -7,7 +7,14 @@ namespace PadawansTask4
         public static int GetVowelCount(string str)
         {
             // put your code here
-            throw new NotImplementedException();
+            str.Replace(" ", "");
+            int count = 0;
+            for(int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')
+                    count++;
+            }
+            return count;
         }
     }
 }
